83f8f7bb636accf6f18ad31bb08312741dc715a2
nichtwal <nekish-legenda@yandex.ru>
2024-06-11 14:20:50 +0300

    new deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'  // Директория для хранения ассетов
-  }
+  base: 'dynamic_app',
 });
